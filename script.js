c4b9444a44f507077a82c9fc340eedf930e06122
MarcosGTS <marcosgtsodre@gmail.com>
2022-11-30 14:34:54 -0300

    add matrix rotation

--- script.js
+++ script.js
@@ -25,6 +25,29 @@ function createGrid() {
     return grid
 }
 
+function matrixRotation(matrix) {
+    const newMatrix = []
+    for (let row = 0; row < matrix.length; row++) {
+        const newRow = []
+        for (let col = 0; col < matrix[0].length; col++) {
+            newRow.unshift(matrix[col][row])
+        }
+        newMatrix.push(newRow)
+    }
+
+    return newMatrix
+}
+
+function rotateBy (matrix, num = 1) {
+    
+    for (let i = 0; i < num; i++) {
+        matrix = matrixRotation(matrix)
+    }
+
+    return matrix
+}
+
+
 const game = initGame(context)
 game.update()
 
